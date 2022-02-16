9daed24410e995c585c961c072af370defe6ee53
Cat-Hanbit <gasida.seo@gmail.com>
2022-02-16 14:13:56 +0900

    calico lab

--- 3/master.sh
+++ 3/master.sh
@@ -40,7 +40,8 @@ ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
 git clone https://github.com/jonmosco/kube-ps1.git /root/kube-ps1
 cat <<"EOT" >> ~/.bash_profile
 source /root/kube-ps1/kube-ps1.sh
-KUBE_PS1_SYMBOL_ENABLE=false
+KUBE_PS1_SYMBOL_ENABLE=true
+KUBE_PS1_SYMBOL_DEFAULT=🐤
 function get_cluster_short() {
   echo "$1" | cut -d . -f1
 }
