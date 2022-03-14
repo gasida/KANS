742940b244c4b20d82d0e44760484a84c12f360f
Cat-Hanbit <gasida.seo@gmail.com>
2022-03-14 20:53:30 +0900

    calico lab

--- 3/init_cfg.sh
+++ 3/init_cfg.sh
@@ -1,58 +1,62 @@
 #!/usr/bin/env bash
 
-# root password
-echo ">>>> root password <<<<<<"
-printf "qwe123\nqwe123\n" | passwd
+echo ">>>> Initial Config Start <<<<"
+echo "[TASK 1] Setting Root Password"
+printf "qwe123\nqwe123\n" | passwd >/dev/null 2>&1
 
-# config sshd
-echo ">>>> ssh-config <<<<<<"
+echo "[TASK 2] Setting Sshd Config"
 sed -i "s/^PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 sed -i "s/^#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
 systemctl restart sshd
 
-# profile bashrc settting
+echo "[TASK 3] Change Timezone & Install Ccat & Setting Profile & Bashrc"
+# Change Timezone
+ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
+# Install Ccat
+curl -s -L https://github.com/owenthereal/ccat/releases/download/v1.1.0/linux-amd64-1.1.0.tar.gz | tar -xz  -C /tmp/
+cp /tmp/linux-amd64-1.1.0/ccat /usr/local/bin/
+chmod +x /usr/local/bin/ccat
+#  Setting Profile & Bashrc
 echo 'alias vi=vim' >> /etc/profile
 echo "sudo su -" >> .bashrc
+echo 'alias cat=/usr/local/bin/ccat' >> /etc/profile
 
-# Letting iptables see bridged traffic
-cat <<EOF >  /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-EOF
-sysctl --system
+echo "[TASK 4] Disable AppArmor"
+systemctl stop ufw && systemctl disable ufw >/dev/null 2>&1
+systemctl stop apparmor && systemctl disable apparmor >/dev/null 2>&1
 
-# local dns setting
+echo "[TASK 5] Install Packages"
+apt update -qq >/dev/null 2>&1
+apt-get install sshpass bridge-utils net-tools jq tree resolvconf wireguard ngrep ipset iputils-arping ipvsadm -y -qq >/dev/null 2>&1
+
+echo "[TASK 6] Change DNS Server IP Address"
+echo -e "nameserver 1.1.1.1" > /etc/resolvconf/resolv.conf.d/head
+resolvconf -u
+
+echo "[TASK 7] Setting Local DNS Using Hosts file"
 echo "192.168.10.10 k8s-m" >> /etc/hosts
 echo "192.168.20.100 k8s-w0" >> /etc/hosts
 for (( i=1; i<=$1; i++  )); do echo "192.168.10.10$i k8s-w$i" >> /etc/hosts; done
 
-# apparmor disable
-systemctl stop apparmor && systemctl disable apparmor
+echo "[TASK 8] Install Docker Engine"
+curl -fsSL https://get.docker.com | sh >/dev/null 2>&1
 
-# package install
-apt update
-apt-get install bridge-utils net-tools jq tree resolvconf wireguard ipset -y
-
-# config dnsserver ip
-echo -e "nameserver 1.1.1.1" > /etc/resolvconf/resolv.conf.d/head
-resolvconf -u
-
-# docker install
-curl -fsSL https://get.docker.com | sh
-
-# Cgroup Driver systemd
-cat <<EOF | tee /etc/docker/daemon.json
+echo "[TASK 9] Change Cgroup Driver Using Systemd"
+cat <<EOT > /etc/docker/daemon.json
 {"exec-opts": ["native.cgroupdriver=systemd"]}
-EOF
-systemctl daemon-reload && systemctl restart docker
+EOT
+systemctl daemon-reload >/dev/null 2>&1
+systemctl restart docker
 
-# swap off
+echo "[TASK 10] Disable and turn off SWAP"
 swapoff -a
 
-# Installing kubeadm kubelet and kubectl
-curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-apt-get update
-apt-get install -y kubelet=1.22.6-00 kubectl=1.22.6-00 kubeadm=1.22.6-00
-apt-mark hold kubelet kubeadm kubectl
+echo "[TASK 11] Install Kubernetes components (kubeadm, kubelet and kubectl) - v$2"
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg >/dev/null 2>&1
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+apt-get update >/dev/null 2>&1
+apt-get install -y kubelet=$2-00 kubectl=$2-00 kubeadm=$2-00 >/dev/null 2>&1
+apt-mark hold kubelet kubeadm kubectl >/dev/null 2>&1
 systemctl enable kubelet && systemctl start kubelet
+
+echo ">>>> Initial Config End <<<<"
--- 3/worker.sh
+++ 3/worker.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/env bash
 
-# config for work_nodes only 
-kubeadm join --token 123456.1234567890123456 --discovery-token-unsafe-skip-ca-verification 192.168.10.10:6443
+echo ">>>> K8S Node config Start <<<<"
+
+echo "[TASK 1] K8S Controlplane Join - API Server 192.168.10.10" 
+kubeadm join --token 123456.1234567890123456 --discovery-token-unsafe-skip-ca-verification 192.168.10.10:6443 >/dev/null 2>&1
+
+echo "[TASK 2] Config kubeconfig" 
+mkdir -p $HOME/.kube
+sshpass -p "qwe123" scp -o StrictHostKeyChecking=no root@k8s-m:/etc/kubernetes/admin.conf $HOME/.kube/config >/dev/null 2>&1
+
+echo "[TASK 3] Source the completion"
+source <(kubectl completion bash)
+echo 'source <(kubectl completion bash)' >> /etc/profile
+
+echo "[TASK 4] Alias kubectl to k"
+echo 'alias k=kubectl' >> /etc/profile
+echo 'complete -F __start_kubectl k' >> /etc/profile
+
+echo "[TASK 5] Install calicoctl Tool - v$3"
+curl -L https://github.com/projectcalico/calico/releases/download/v$3/calicoctl-linux-amd64 -o calicoctl >/dev/null 2>&1
+chmod +x calicoctl && mv calicoctl /usr/bin
+
+echo ">>>> K8S Node config End <<<<"
