99b21d6ab718f3ee190ad91c3e76f83aec77bb7a
Cat-Hanbit <gasida.seo@gmail.com>
2022-05-13 22:40:55 +0900

    mysql operator for k8s lab

---
+++ mysql/master.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+echo ">>>> K8S Controlplane config Start <<<<"
+
+echo "[TASK 1] Initial Kubernetes - Pod CIDR 172.16.0.0/16 , Service CIDR 10.200.1.0/24 , API Server 192.168.10.10"
+kubeadm init --token 123456.1234567890123456 --token-ttl 0 --pod-network-cidr=172.16.0.0/16 --apiserver-advertise-address=192.168.10.10 --service-cidr 10.200.1.0/24 >/dev/null 2>&1
+
+echo "[TASK 2] Setting kube config file"
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+
+echo "[TASK 3] Install Flannel CNI"
+kubectl apply -f https://raw.githubusercontent.com/gasida/book-k8s-network/main/3/kube-flannel-v0.17.0.yml >/dev/null 2>&1
+
+echo "[TASK 5] Source the completion"
+# source bash-completion for kubectl kubeadm
+source <(kubectl completion bash)
+## Source the completion script in your ~/.bashrc file
+echo 'source <(kubectl completion bash)' >> /etc/profile
+
+echo "[TASK 6] Alias kubectl to k"
+echo 'alias k=kubectl' >> /etc/profile
+echo 'complete -F __start_kubectl k' >> /etc/profile
+
+echo "[TASK 7] Install Kubectx & Kubens"
+git clone https://github.com/ahmetb/kubectx /opt/kubectx >/dev/null 2>&1
+ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+
+echo "[TASK 8] Install Kubeps & Setting PS1"
+git clone https://github.com/jonmosco/kube-ps1.git /root/kube-ps1 >/dev/null 2>&1
+cat <<"EOT" >> ~/.bash_profile
+source /root/kube-ps1/kube-ps1.sh
+KUBE_PS1_SYMBOL_ENABLE=false
+function get_cluster_short() {
+  echo "$1" | cut -d . -f1
+}
+KUBE_PS1_CLUSTER_FUNCTION=get_cluster_short
+KUBE_PS1_SUFFIX=') '
+PS1='$(kube_ps1)'$PS1
+EOT
+kubectl config rename-context "kubernetes-admin@kubernetes" "$1-Lab" >/dev/null 2>&1
+
+echo "[TASK 9] Install Packages"
+apt install kubetail etcd-client mariadb-client -y -qq >/dev/null 2>&1
+
+echo "[TASK 10] Install Helm"
+curl -s https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash >/dev/null 2>&1
+
+echo "[TASK 11] Install Metrics server - v0.6.1"
+kubectl apply -f https://raw.githubusercontent.com/gasida/KANS/main/8/metrics-server.yaml >/dev/null 2>&1
+
+echo "[TASK 12] Dynamically provisioning persistent local storage with Kubernetes - v0.0.22"
+kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml >/dev/null 2>&1
+kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}' >/dev/null 2>&1
+
+echo ">>>> K8S Controlplane Config End <<<<"
